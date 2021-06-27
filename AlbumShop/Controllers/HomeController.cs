c44ecaca93c576c08879729db549081cab503958
udovenkodima7@gmail.com <udovenkodima7@gmail.com>
2021-06-27 15:50:28 +0300

    HomeViewModel

--- AlbumShop/Controllers/HomeController.cs
+++ AlbumShop/Controllers/HomeController.cs
@@ -1,11 +1,24 @@
-﻿using System;
+﻿using AlbumShop.Data.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace AlbumShop.Controllers
 {
-    public class HomeController
+    public class HomeController : Controller
     {
+        private IAllAlbums _albumRep;
+        
+        public HomeController(IAllAlbums albRep)
+        {
+            _albumRep = albRep;
+            
+        }
+        public ViewResult Index()
+        {
+            return View();
+        }
     }
 }
