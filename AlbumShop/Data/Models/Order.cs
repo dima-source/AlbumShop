e20fd77ed52010d241e9298c593243fa3e5cf19e
udovenkodima7@gmail.com <udovenkodima7@gmail.com>
2021-07-01 17:52:08 +0300

    Order

---
+++ AlbumShop/Data/Models/Order.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlbumShop.Data.Models
+{
+    public class Order
+    {
+    }
+}
---
+++ AlbumShop/Data/Models/OrderDetail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlbumShop.Data.Models
+{
+    public class OrderDetail
+    {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public int AlbumId { get; set; }
+        public int Price { get; set; }
+        
+        public virtual Album Album { get; set; }
+        public virtual Order Order { get; set; }
+    }
+}
