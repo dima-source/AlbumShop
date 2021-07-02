54c43a8b44e3465f35ef7c25a21646eb57753606
udovenkodima7@gmail.com <udovenkodima7@gmail.com>
2021-07-02 16:35:41 +0300

    AllOdersContrpllerConst

--- AlbumShop/Controllers/OrderController.cs
+++ AlbumShop/Controllers/OrderController.cs
@@ -1,11 +1,21 @@
-﻿using System;
+﻿using AlbumShop.Data;
+using AlbumShop.Data.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace AlbumShop.Controllers
 {
-    public class OrderController
+    public class OrderController : Controller
     {
+        private readonly IAllOrders allOrders;
+        private readonly ShopCart shopCart;
+        public OrderController(IAllOrders allOrders, ShopCart shopCart)
+        {
+            this.allOrders = allOrders;
+            this.shopCart = shopCart;
+        }
     }
 }
