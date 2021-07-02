b0849c3eac2de0f4a216bcd04d192dc1bc1c6ee2
udovenkodima7@gmail.com <udovenkodima7@gmail.com>
2021-07-02 16:45:43 +0300

    AllCom

--- AlbumShop/Data/Interfaces/IAllOrders.cs
+++ AlbumShop/Data/Interfaces/IAllOrders.cs
@@ -1,11 +1,13 @@
-﻿using System;
+﻿using AlbumShop.Data.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace AlbumShop.Data.Interfaces
 {
-    interface IAllOrders
+    public interface IAllOrders
     {
+        void createOrder(Order order);
     }
 }
