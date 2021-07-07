0e557e72608ce093f878e032efefc267a30356d3
udovenkodima7@gmail.com <udovenkodima7@gmail.com>
2021-07-07 15:41:12 +0300

    AddToDataBaseViewModel

---
+++ AlbumShop/ViewModels/AddToDataBaseViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlbumShop.ViewModels
+{
+    public class AddToDataBaseViewModel
+    {
+    }
+}
