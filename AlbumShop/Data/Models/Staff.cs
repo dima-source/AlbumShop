e5807fcd2e53aee24e0589388eb21c030923a381
udovenkodima7@gmail.com <udovenkodima7@gmail.com>
2021-07-08 17:29:11 +0300

    StaffModel

---
+++ AlbumShop/Data/Models/Staff.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlbumShop.Data.Models
+{
+    public class Staff
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string SuName { get; set; }
+        public string IMG { get; set; }
+        public string Desc { get; set; }
+        public string Position { get; set; }
+    }
+}
